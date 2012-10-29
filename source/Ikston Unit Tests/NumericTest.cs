1d6e61263ffea0e69d3538f03d6314b328bfb366
Ivan Kravar??an <Ivan Kravar??an@localhost>
2012-10-29 23:05:46 +0100

    - Added unit test project - Added unit tests for IKSTON textual values - Added unit tests for IKSTON numeric values

---
+++ source/Ikston Unit Tests/NumericTest.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ikon;
+using System.IO;
+using Ikon.Ston.Values;
+using System.Globalization;
+
+namespace Ikston_Unit_Tests
+{
+	[TestClass]
+	public class NumericTest
+	{
+		[TestMethod]
+		public void NumericType()
+		{
+			string input = "=2";
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext();
+
+			Assert.AreEqual(Numeric.ValueTypeName, value.TypeName);
+		}
+
+		[TestMethod]
+		public void NumericReadValueShort()
+		{
+			short expectedValue = 2;
+			string input = "=" + expectedValue;
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetShort);
+		}
+
+		[TestMethod]
+		public void NumericReadValueInt()
+		{
+			int expectedValue = 1234567;
+			string input = "=" + expectedValue;
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetInt);
+		}
+
+		[TestMethod]
+		public void NumericReadValueIntNegative()
+		{
+			int expectedValue = -1234567;
+			string input = "=" + expectedValue;
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetInt);
+		}
+
+		[TestMethod]
+		public void NumericReadValueLong()
+		{
+			long expectedValue = 123456789012L;
+			string input = "=" + expectedValue;
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetLong);
+		}
+
+		[TestMethod]
+		public void NumericReadValueDecimal()
+		{
+			decimal expectedValue = 1.23456789012m;
+			string input = "=" + expectedValue.ToString(NumberFormatInfo.InvariantInfo);
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetDecimal);
+		}
+
+		[TestMethod]
+		public void NumericReadValueFloat()
+		{
+			float expectedValue = 1.2345f;
+			string input = "=" + expectedValue.ToString(NumberFormatInfo.InvariantInfo);
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetFloat);
+		}
+
+		[TestMethod]
+		public void NumericReadValueDouble()
+		{
+			double expectedValue = 1.23456789;
+			string input = "=" + expectedValue.ToString(NumberFormatInfo.InvariantInfo);
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetDouble);
+		}
+
+		[TestMethod]
+		public void NumericReadValueDoubleNegative()
+		{
+			double expectedValue = -1.23456789;
+			string input = "=" + expectedValue.ToString(NumberFormatInfo.InvariantInfo);
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetDouble);
+		}
+
+		[TestMethod]
+		public void NumericReadValueDoubleInfinity()
+		{
+			double expectedValue = double.PositiveInfinity;
+			string input = "=" + expectedValue.ToString(NumberFormatInfo.InvariantInfo);
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetDouble);
+		}
+
+		[TestMethod]
+		public void NumericReadValueDoubleNegativeInfinity()
+		{
+			double expectedValue = double.NegativeInfinity;
+			string input = "=" + expectedValue.ToString(NumberFormatInfo.InvariantInfo);
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetDouble);
+		}
+
+		[TestMethod]
+		public void NumericReadValueDoubleNaN()
+		{
+			double expectedValue = double.NaN;
+			string input = "=" + expectedValue.ToString(NumberFormatInfo.InvariantInfo);
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Numeric;
+
+			Assert.AreEqual(expectedValue, value.GetDouble);
+		}
+	}
+}
---
+++ source/Ikston Unit Tests/TextTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Ikon.Ston.Values;
+using Ikon;
+
+namespace Ikston_Unit_Tests
+{
+	[TestClass]
+	public class TextTests
+	{
+		const char Q = '"';
+
+		[TestMethod]
+		public void TextType()
+		{
+			string input = Q + "some text" + Q;
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext();
+
+			Assert.AreEqual(value.TypeName, Text.ValueTypeName);
+		}
+
+		[TestMethod]
+		public void TextReadValueSimple()
+		{
+			string inputValue = "some text";
+			string input = Q + inputValue + Q;
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Text;
+
+			Assert.AreEqual(value.GetText, inputValue);
+		}
+		
+		[TestMethod]
+		public void TextReadValueEscapes()
+		{
+			string inputValue = "some text\\nnew line\\ttab\\n\\ranother line and \\\" qoute";
+			string expectedValue = "some text\nnew line\ttab\n\ranother line and \" qoute";
+			string input = Q + inputValue + Q;
+
+			Parser parser = new Ikon.Ston.Parser(new StringReader(input));
+			var value = parser.ParseNext() as Text; 
+
+			Assert.AreEqual(value.GetText, expectedValue);
+		}
+
+		[TestMethod]
+		public void TextWriteValueSimple()
+		{
+			string inputValue = "some text";
+			string ikonData = Q + inputValue + Q;
+			StringBuilder output = new StringBuilder();
+
+			IkonWriter writer = new IkonWriter(new StringWriter(output));
+			var value = new Text(inputValue);
+			value.Compose(writer);
+
+			Assert.AreEqual(ikonData, output.ToString().Trim());
+		}
+
+		[TestMethod]
+		public void TextWriteValueEscapes()
+		{
+			string rawIkonData = "some text\\nnew line\\ttab\\n\\ranother line and \\\" qoute";
+			string inputValue = "some text\nnew line\ttab\n\ranother line and \" qoute";
+			string ikonData = Q + rawIkonData + Q;
+			StringBuilder output = new StringBuilder();
+
+			IkonWriter writer = new IkonWriter(new StringWriter(output));
+			var value = new Text(inputValue);
+			value.Compose(writer);
+
+			Assert.AreEqual(ikonData, output.ToString().Trim());
+		}
+	}
+}
